4f539321352cca8fae1de533b3c4932b66f656b9
sami2316 <sami2316@users.noreply.github.com>
2015-06-20 21:32:08 +0500

    Update inquiry.py

--- examples/simple/inquiry.py
+++ examples/simple/inquiry.py
@@ -10,7 +10,7 @@ import bluetooth
 print("performing inquiry...")
 
 nearby_devices = bluetooth.discover_devices(
-        duration=8, lookup_names=True, flush_cache=True, lookup_class=False)
+        duration=8, lookup_names=True, flush_cache=True )
 
 print("found %d devices" % len(nearby_devices))
 
